38bdf9aca8568cfb81667187e55deaeb0c5851de
keita <kmahamadou858@gmail.com>
2021-12-21 10:49:12 +0000

    fin du projet atelier

--- models/client.py
+++ models/client.py
@@ -1,27 +1,31 @@
-#-*- coding: utf-8 -*-
+#-- coding: utf-8 --
 from odoo import models, fields, api
 
 class client(models.Model):
     _inherit = "res.partner"
     _name = "res.partner"
     _description = "client"
-
+    #Commun
     dos = fields.Float(string="Dos")
     epaule = fields.Float("Epaule")
     epaulemache = fields.Float("Epaule Mache")
     poitrine = fields.Float("poitrine")
-    taille  = fields.Float("T.Taille")
+    ceinture = fields.Float("Ceinture")
     longs = fields.Float("Long.S")
     longt = fields.Float("Long.T")
+    longpantalon = fields.Float("Long.Pantalon")
+    cuisse = fields.Float("Cuisse")
+    bas = fields.Float("Bas")
+    model = fields.Char("Model")
+    #Homme
+    coup = fields.Float("coup")
+    patte = fields.Float("patte")
+    #Femme
+    taille = fields.Float("taille")
     bassin = fields.Float("Bassin")
     manche = fields.Float("Manche")
     longhaut = fields.Float("Long.Haut")
-    ceinture = fields.Float("Ceinture")
-    longpantalon = fields.Float("Long.Pantalon")
     longrobe = fields.Float("Long.Robe")
-    cuisse = fields.Float("Cuisse")
     longjup = fields.Float("Long.Jube")
-    bas = fields.Float("Bas")
-    model = fields.Char("Model")
-    sexe = fields.Selection(selection=[('masculin', 'Masculin'), ('feminin', 'Feminin')])
 
+    sexe = fields.Selection(selection=[('masculin', 'Masculin'), ('feminin', 'Feminin')],default="feminin")
